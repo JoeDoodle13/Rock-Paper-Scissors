935775d7c4ae5ac3730b0e6dbcb0b3aec7858267
Joe <lerchejoseph@gmail.com>
2022-04-19 10:15:48 -0500

    Randomize Computer Selection and comparing values
    Implemented the computers choice into the code, and now the beginning
    process of comparing rock vs paper vs scissors. Because strings in an
    array hold the value of a specific number, these numbers are going to be
    compared. Same values like 1-1 are already omitted (draw); so now just
    work with 0-2, 0-1.

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const choices = ["rock", "paper", "scissors", "dynamite"];
+const myChoices = ["rock", "paper", "scissors", "dynamite"];
+const compChoices = ["rock", "paper", "scissors"];
 
+// propmts and checks users choice
 function mySelectFunc(choice) {
     let mySelect = prompt("What would you like to throw? Rock, Paper, or Scissors?")
-    let isChoice = choices.includes(mySelect.toLowerCase());
+    let isChoice = myChoices.includes(mySelect.toLowerCase());
 
     isChoice_Check(isChoice)
     function isChoice_Check() {
         if (isChoice !== true) {
         mySelect = prompt('Hint: Throw "Rock", "Paper", or "Scissors"');
-        isChoice = choices.includes(mySelect.toLowerCase());
+        isChoice = myChoices.includes(mySelect.toLowerCase());
         isChoice_Check(isChoice);
     }}
-    return choice = mySelect;
+    return choice = mySelect.toLowerCase();
 }
-console.log(mySelectFunc())
+mySelect = mySelectFunc();
+
+
+// random select for computer
+function compSelect(choice) {
+    return choice = "Paper";//choices[Math.floor(Math.random() * choices.length)];
+}
+let result = mySelect === compSelect().toLowerCase();
+
+console.log(`Computer threw ${compSelect()}.`, `You threw ${mySelect}`);
+
+if (result) {
+    console.log("Draw, redo");
+} else {
+    result = '' + myChoices.indexOf(`${mySelect}`) + compChoices.indexOf(`${compSelect}`);
+
+    console.log(result)
+
+}
+    /*now, in this block, include the rules of the game;
+    rock beats scissors, scissors beat paper, paper beats rock
+    
+    smth like `${winners selection} beats ${losers selection}; ${winner} won`*/
+
+    
+
+
