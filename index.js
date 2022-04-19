7ad8dd1190f8c122d7d00a825dc29db9ad9048bc
Joe <lerchejoseph@gmail.com>
2022-04-19 09:33:10 -0500

    isChoice now compares mySelect to choices

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors", "dynamite"];
 
 function mySelectFunc(choice) {
     let mySelect = prompt("What would you like to throw? Rock, Paper, or Scissors?")
-    let isChoice = mySelect.toLowerCase() === ("rock" || "paper" || "scissors");
+    let isChoice = choices.includes(mySelect.toLowerCase());
 
     isChoice_Check(isChoice)
     function isChoice_Check() {
         if (isChoice !== true) {
         mySelect = prompt('Hint: Throw "Rock", "Paper", or "Scissors"');
-        isChoice = mySelect.toLowerCase() === ("rock" || "paper" || "scissors");
+        isChoice = choices.includes(mySelect.toLowerCase());
         isChoice_Check(isChoice);
     }}
     return choice = mySelect;
 }
 console.log(mySelectFunc())
-
-/*
-isChoice_Check(isChoice);
-function isChoice_Check(isChoice) {
-    if (isChoice !== true) {
-        mySelect = 
-        isChoice_Check(mySelect) 
-    } else {
-    return mySelect;
-    }
-}*/
