509d3aeaedfca07b67c319ef955872958a84c781
Joe <lerchejoseph@gmail.com>
2022-04-18 22:34:33 -0500

    Add "if else" statement, compare reseults
    If both players choose the same 'weapon', there's no winner. But if
    the selections are different, there is one. I haven't included that,
    but included a comment to where I'd like to go

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const choices = ["Rock", "Paper", "Scissor"]
+const choices = ["Rock", "Paper", "Scissor"];
 
+let mySelection = prompt("What would you like to throw? Rock, Paper, or Scissors?")
 function computerSelection(choice) {
-    choice = choices[Math.floor(Math.random() * choices.length)];
-    return choice;
+    return choice = "Paper";//choices[Math.floor(Math.random() * choices.length)];
 }
+let result = mySelection.toLowerCase() === computerSelection().toLowerCase();
 
-console.log(computerSelection());
+console.log(`Computer threw ${computerSelection()}.`, `You threw ${mySelection}`);
+
+if (result) {
+    console.log("Draw, redo");
+} else {
+    console.log("Joe won")
+}
