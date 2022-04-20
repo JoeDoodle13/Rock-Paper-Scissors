6493b320f4fbeabc73905f85a1e5da8cae62161c
Joe <lerchejoseph@gmail.com>
2022-04-19 21:24:24 -0500

    Finish game, scoring system
    The majority of the code, from choosing an answer to comparing the
    values, was put under a function, round(). After each round, either
    the User or the Computer wins a point (incrementing). Whoever wins 2
    points, wins the game and the code ends. (Only one problem, the winner
    is displayed twice.)

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
 const compChoices = ["rock", "paper", "scissors"];
 const myChoices = ["rock", "paper", "scissors", "dynamite"];
-const compWin = ["02", "21", "10"];
-const myWin = ["20", "12", "01", "23", "13", "03"];
+const compWins = ["02", "21", "10"];
+const myWins = ["20", "12", "01", "23", "13", "03"];
+let compWinsTotal = 0;
+let myWinsTotal = 0;
 
 // propmts and checks users choice
-function mySelectFunc(choice) {
-    let mySelect = prompt("What would you like to throw? Rock, Paper, or Scissors?")
-    let isChoice = myChoices.includes(mySelect.toLowerCase());
+function round() {
+    function mySelectFunc(choice) {
+        let mySelect = prompt("What would you like to throw? Rock, Paper, or Scissors?")
+        let isChoice = myChoices.includes(mySelect.toLowerCase());
 
-    isChoice_Check(isChoice)
-    function isChoice_Check() {
-        if (isChoice !== true) {
-        mySelect = prompt('Hint: Throw "Rock", "Paper", or "Scissors"');
-        isChoice = myChoices.includes(mySelect.toLowerCase());
-        isChoice_Check(isChoice);
-    }}
-    return choice = mySelect.toLowerCase();
-}
-mySelect = mySelectFunc();
-
-
-// random select for computer
-function compSelectFunc(choice) {
-    return choice = compChoices[Math.floor(Math.random() * compChoices.length)];
-}
-let compSelect = compSelectFunc().toLowerCase();
-let result = mySelect === compSelect;
-console.log(`Computer threw ${compSelect}.`, `You threw ${mySelect}`);
-
-if (result) {
-    console.log("Draw, redo");
-} else {
-    result = '' + compChoices.indexOf(`${compSelect}`) + myChoices.indexOf(`${mySelect}`);
-}
-
-winnerFunc(result)
-function winnerFunc() {
-    let compWinner = compSelect[0].toUpperCase() + compSelect.slice(1);
-    let myWinner = mySelect[0].toUpperCase() + mySelect.slice(1);
-    if (compWin.includes(result) === true) {
-        console.log(`${compWinner} beats ${myWinner}, the Computer wins`)
-    } else if (myWin.includes(result) === true) {
-        console.log(`${myWinner} beats ${compWinner}, the User wins`)
-    } else {
-        console.log("Error, try again");
-
+        isChoice_Check(isChoice)
+        function isChoice_Check() {
+            if (isChoice !== true) {
+            mySelect = prompt('Hint: Throw "Rock", "Paper", or "Scissors"');
+            isChoice = myChoices.includes(mySelect.toLowerCase());
+            isChoice_Check(isChoice);
+        }}
+        return choice = mySelect.toLowerCase();
     }
-}
+    mySelect = mySelectFunc();
 
 
-    
+    // random select for computer
+    function compSelectFunc(choice) {
+        return choice = compChoices[Math.floor(Math.random() * compChoices.length)];
+    }
+    let compSelect = compSelectFunc().toLowerCase();
+    let result = mySelect === compSelect;
+    console.log(`Computer threw ${compSelect}.`, `You threw ${mySelect}`);
+
+    if (result) {
+        console.log("Draw, redo");
+        alert("Draw, redo");
+        console.clear();
+        round();
+    } else {
+        result = '' + compChoices.indexOf(`${compSelect}`) + myChoices.indexOf(`${mySelect}`);
+    }
+
+    winnerFunc(result)
+    function winnerFunc() {
+        let compWinner = compSelect[0].toUpperCase() + compSelect.slice(1);
+        let myWinner = mySelect[0].toUpperCase() + mySelect.slice(1);
+        if (compWins.includes(result) === true) {
+            console.log(`${compWinner} beats ${myWinner}, the Computer wins`);
+            compWinsTotal++;
+        } else if (myWins.includes(result) === true) {
+            console.log(`${myWinner} beats ${compWinner}, the User wins`)
+            myWinsTotal++;
+        }
+    }
+
+    switch (true) {
+        case (myWinsTotal === 2):
+            console.log("Joe Wins");
+            break;
+        
+        case (compWinsTotal === 2):
+            console.log("Computer Wins");
+            break;
+
+        default:
+            round()
+    }
+};    
+
+round()
+
     /*now, in this block, include the rules of the game;
     rock beats scissors, scissors beat paper, paper beats rock
     
